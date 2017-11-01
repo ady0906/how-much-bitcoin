d1340665d0cd057b595d9c98dae6440d3264d778
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-01 23:48:13 +0100

    building initial component

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/laurencet/lighthouse/how-much-bitcoin/pages/index.js?entry";
+
+var Index = function Index() {
+    return _react2.default.createElement("div", {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 2
+        }
+    }, _react2.default.createElement("p", {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 3
+        }
+    }, "Building a React component"));
+};
+
+exports.default = Index;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkluZGV4Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFBLElBQU0sUUFBUSxTQUFSLEFBQVEsUUFBQTsyQkFDVixjQUFBOztzQkFBQTt3QkFBQSxBQUNJO0FBREo7QUFBQSxLQUFBLGtCQUNJLGNBQUE7O3NCQUFBO3dCQUFBO0FBQUE7QUFBQSxPQUZNLEFBQ1YsQUFDSTtBQUZSLEFBTUE7O2tCQUFBLEFBQWUiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2xhdXJlbmNldC9saWdodGhvdXNlL2hvdy1tdWNoLWJpdGNvaW4ifQ==
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => (
+    <div>
+        <p>Building a React component</p>
+    </div>
+)
+
+export default Index
