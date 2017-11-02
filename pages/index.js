88b5c798fab5d35bab9305e9aa248f24e5d7df5d
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-02 22:53:09 +0100

    passing data through querystrings in react

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
 import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+
+const PostLink = (props) => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
 
 export default () => (
     <Layout>
-        <p>Hello Next.js</p>
+        <h1>Adrien's blog</h1>
+        <ul>
+            <PostLink title='Hello Next.js'/>
+            <PostLink title='Learning Next.js + React is the bomb'/>
+            <PostLink title='Deploy apps with Zeit'/>
+        </ul>
     </Layout>
 )
