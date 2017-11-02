7874ba824b74ae833b6177ffd6893f7542fc4a40
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-02 21:37:48 +0100

    gitignoring next related files + passing props to Layout component

--- .next/dist/pages/about.js
+++ .next/dist/pages/about.js
@@ -1,28 +1,32 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _react = require("react");
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _MyLayout = require('../components/MyLayout.js');
+
+var _MyLayout2 = _interopRequireDefault(_MyLayout);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _jsxFileName = "/Users/laurencet/lighthouse/how-much-bitcoin/pages/about.js?entry";
+var _jsxFileName = '/Users/laurencet/lighthouse/how-much-bitcoin/pages/about.js?entry';
 
 exports.default = function () {
-    return _react2.default.createElement("div", {
+    return _react2.default.createElement(_MyLayout2.default, {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 2
+            lineNumber: 4
         }
-    }, _react2.default.createElement("p", {
+    }, _react2.default.createElement('p', {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 3
+            lineNumber: 5
         }
-    }, "This is an about page"));
+    }, 'This is an about page'));
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2Fib3V0LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7Ozs7OztrQkFBZSxZQUFBOzJCQUNYLGNBQUE7O3NCQUFBO3dCQUFBLEFBQ0k7QUFESjtBQUFBLEtBQUEsa0JBQ0ksY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLE9BRk8sQUFDWCxBQUNJO0FBRlIiLCJmaWxlIjoiYWJvdXQuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2xhdXJlbmNldC9saWdodGhvdXNlL2hvdy1tdWNoLWJpdGNvaW4ifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2Fib3V0LmpzIl0sIm5hbWVzIjpbIkxheW91dCJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLEFBQU8sQUFBWSxBQUVuQjs7Ozs7Ozs7a0JBQWUsWUFBQTsyQkFDWCxBQUFDOztzQkFBRDt3QkFBQSxBQUNJO0FBREo7QUFBQSxLQUFBLGtCQUNJLGNBQUE7O3NCQUFBO3dCQUFBO0FBQUE7QUFBQSxPQUZPLEFBQ1gsQUFDSTtBQUZSIiwiZmlsZSI6ImFib3V0LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9sYXVyZW5jZXQvbGlnaHRob3VzZS9ob3ctbXVjaC1iaXRjb2luIn0=
--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -8,30 +8,25 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _Header = require('../components/Header');
+var _MyLayout = require('../components/MyLayout.js');
 
-var _Header2 = _interopRequireDefault(_Header);
+var _MyLayout2 = _interopRequireDefault(_MyLayout);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = '/Users/laurencet/lighthouse/how-much-bitcoin/pages/index.js?entry';
 
 exports.default = function () {
-    return _react2.default.createElement('div', {
+    return _react2.default.createElement(_MyLayout2.default, {
         __source: {
             fileName: _jsxFileName,
             lineNumber: 4
         }
-    }, _react2.default.createElement(_Header2.default, {
+    }, _react2.default.createElement('p', {
         __source: {
             fileName: _jsxFileName,
             lineNumber: 5
         }
-    }), _react2.default.createElement('p', {
-        __source: {
-            fileName: _jsxFileName,
-            lineNumber: 6
-        }
     }, 'Hello Next.js'));
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkhlYWRlciJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLEFBQU8sQUFBWSxBQUVuQjs7Ozs7Ozs7a0JBQWUsWUFBQTsyQkFDWCxjQUFBOztzQkFBQTt3QkFBQSxBQUNJO0FBREo7QUFBQSxLQUFBLGtCQUNJLEFBQUM7O3NCQUFEO3dCQURKLEFBQ0ksQUFDQTtBQURBO0FBQUEsd0JBQ0EsY0FBQTs7c0JBQUE7d0JBQUE7QUFBQTtBQUFBLE9BSE8sQUFDWCxBQUVJO0FBSFIiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2xhdXJlbmNldC9saWdodGhvdXNlL2hvdy1tdWNoLWJpdGNvaW4ifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkxheW91dCJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLEFBQU8sQUFBWSxBQUVuQjs7Ozs7Ozs7a0JBQWUsWUFBQTsyQkFDWCxBQUFDOztzQkFBRDt3QkFBQSxBQUNJO0FBREo7QUFBQSxLQUFBLGtCQUNJLGNBQUE7O3NCQUFBO3dCQUFBO0FBQUE7QUFBQSxPQUZPLEFBQ1gsQUFDSTtBQUZSIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9sYXVyZW5jZXQvbGlnaHRob3VzZS9ob3ctbXVjaC1iaXRjb2luIn0=
