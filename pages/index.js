866446c4198700185100c6e1db805016b869e407
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-05 19:47:02 +0100

    displaying results from API call on index page

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ const Index = (props) => (
 
 // set initial values based on API data
 Index.getInitialProps = async function() {
-    const res = await fetch('https//api.tvmaze.com/search/shows?q=batman')
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
     const data = await res.json()
 
     console.log(`Show data fetched. Count: ${data.length}`)
