811816794bb2b2566edec1da09b197a66b81fdcb
adrien.peynichou@gmail.com <adrien.peynichou@gmail.com>
2017-11-14 15:48:46 -0500

    adding link to personal page + formatting footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,12 +6,16 @@ const footerStyle = {
 
 const Footer = () => (
     <div>
-        <Link href='https://www.coindesk.com/price/'>
-            <a>⚡ by CoinDesk</a>
-        </Link>
-        <Link>
-            <a>Built with ♥ by Adrien Peynichou</a>
-        </Link>
+        <p>
+            <Link href='https://www.coindesk.com/price/'>
+                <a>⚡ by CoinDesk</a>
+            </Link>
+        </p>
+        <p>
+            <Link href='http://www.adrienpeynichou.com'>
+                <a>Built with ♥ by Adrien Peynichou</a>
+            </Link>
+        </p>
     </div>
 )
 
